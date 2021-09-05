ec975c6352219f6e621de12683523b3b33458cbc
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-05 14:29:57 +0400

    [+][ADD] Add lazy column to display data

--- app/src/main/java/com/ziyaad/profilecardlayout/UserProfile.kt
+++ app/src/main/java/com/ziyaad/profilecardlayout/UserProfile.kt
@@ -5,4 +5,10 @@ data class UserProfile(val name: String, val status: Boolean, val drawableId: In
 val userProfileList = arrayListOf<UserProfile>(
     UserProfile(name = "John Doe", status = true, R.drawable.profilepicture),
     UserProfile(name = "Jessica Doe", status = false, R.drawable.profilepicture),
+    UserProfile(name = "John Doe", status = true, R.drawable.profilepicture),
+    UserProfile(name = "Jessica Doe", status = false, R.drawable.profilepicture),
+    UserProfile(name = "John Doe", status = true, R.drawable.profilepicture),
+    UserProfile(name = "Jessica Doe", status = false, R.drawable.profilepicture),
+    UserProfile(name = "John Doe", status = true, R.drawable.profilepicture),
+    UserProfile(name = "Jessica Doe", status = false, R.drawable.profilepicture),
 )
