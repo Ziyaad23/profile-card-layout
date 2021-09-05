9bb76e2ee349e3a73c5e944dabef234077153625
Ziyaad <rhyman.ziyaad@gmail.com>
2021-09-05 09:58:20 +0400

    [+][ADD] Create skeleton for profile card layout

--- app/src/main/java/com/ziyaad/profilecardlayout/MainActivity.kt
+++ app/src/main/java/com/ziyaad/profilecardlayout/MainActivity.kt
@@ -3,36 +3,63 @@ package com.ziyaad.profilecardlayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.Card
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.ziyaad.profilecardlayout.ui.theme.ProfileCardLayoutTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProfileCardLayoutTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = Color.LightGray
+    ) {
+        ProfileCard()
+    }
+}
+
+@Composable
+fun ProfileCard() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        elevation = 8.dp
+    ) {
+        Row(modifier = Modifier.wrapContentSize()) {
+            ProfilePicture()
+            ProfileContent()
+        }
+    }
+}
+
+@Composable
+fun ProfilePicture() {
+
+}
+
+@Composable
+fun ProfileContent() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ProfileCardLayoutTheme {
-        Greeting("Android")
-    }
+    ProfilePicture()
 }
